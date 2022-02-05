ec54eeae2ee1abf1af0f5e70fec25d25e400fc8d
Axm <56112475+ax-sh@users.noreply.github.com>
2022-02-06 01:46:40 +0545

    fix: nav menu distance

--- apps/portfolio/src/app/App.tsx
+++ apps/portfolio/src/app/App.tsx
@@ -25,7 +25,7 @@ const Nav = () => {
   const [items] = React.useState(['HOME', 'CONTACT', 'ABOUT']);
   return (
     <nav className="flex justify-end p-4 bg-white text-black">
-      <List className="flex w-80 justify-between ">
+      <List className="flex w-56 justify-between ">
         {items.map((item) => (
           <a className="hover:text-red-500" href="#home">
             {item}
