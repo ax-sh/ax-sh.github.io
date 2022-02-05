f978f5fad7a3b80c53a11898fc2f0177082b424c
Axm <56112475+ax-sh@users.noreply.github.com>
2022-02-06 00:58:47 +0545

    feat: add windicss

--- apps/portfolio/src/main.tsx
+++ apps/portfolio/src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import 'virtual:windi.css';
+// import 'virtual:windi-devtools';
 
 import App from './app/App';
 
