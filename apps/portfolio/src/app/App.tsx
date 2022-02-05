bb43bb4f855e574f9626320995e85f79d998ae23
Axm <56112475+ax-sh@users.noreply.github.com>
2022-02-06 01:20:58 +0545

    fix: serve portfolio form nx

--- apps/portfolio/src/app/App.tsx
+++ apps/portfolio/src/app/App.tsx
@@ -1,4 +1,10 @@
 function App() {
-  return <section>App</section>;
+  return (
+    <main className="app min-h-screen bg-black text-white grid place-content-center">
+      <header className="p-10 rounded-md bg-green-300">
+        <h1 className="text-6xl text-black">AX-SH</h1>
+      </header>
+    </main>
+  );
 }
 export default App;
