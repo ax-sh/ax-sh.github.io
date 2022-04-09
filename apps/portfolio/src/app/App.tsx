5fc4ccc50dbc63f87fb3cd903dd616ab7752f87c
Axm <56112475+ax-sh@users.noreply.github.com>
2022-04-10 00:52:43 +0545

    chore: ove files

--- apps/portfolio/src/app/App.tsx
+++ apps/portfolio/src/app/App.tsx
@@ -1,36 +1,8 @@
 import React from 'react';
-import Obfuscate from 'react-obfuscate';
 
 import { HeroSection } from './containers/HeroSection';
-import { GetIcon } from '@ax-sh.github.io/common';
+import { ExternalLinks } from '@ax-sh.github.io/common';
 import { WorkSection } from './containersWorkSection';
-
-const data = {
-  email: 'example@example.com',
-  // cv: "",
-  links: {
-    Github: 'https://github.com/ax-sh',
-    LinkedIn: 'https://www.linkedin.com/in/axmin/',
-    CodeSandbox: 'https://codesandbox.io/u/ax-sh',
-    Twitter: 'https://twitter.com/ax___sh',
-    Website: 'https://ax-sh.github.io/portfolio/',
-    // CV: "",
-  },
-};
-
-export const ExternalLinks = () => (
-  <div className="links">
-    <ul className="flex w-50 justify-around">
-      {Object.entries(data.links).map(([label, value]) => (
-        <li key={label}>
-          <Obfuscate target="_blank" href={value}>
-            <GetIcon name={label} />
-          </Obfuscate>
-        </li>
-      ))}
-    </ul>
-  </div>
-);
 
 const Layout = ({
   children,
--- libs/common/src/lib/common.tsx
+++ libs/common/src/lib/common.tsx
@@ -1,14 +1,33 @@
 import './common.module.scss';
+import { GetIcon } from './Icons';
+import React from 'react';
+// @ts-ignore
+import Obfuscate from 'react-obfuscate';
 
-/* eslint-disable-next-line */
-export interface CommonProps {}
+const data = {
+  email: 'example@example.com',
+  // cv: "",
+  links: {
+    Github: 'https://github.com/ax-sh',
+    LinkedIn: 'https://www.linkedin.com/in/axmin/',
+    CodeSandbox: 'https://codesandbox.io/u/ax-sh',
+    Twitter: 'https://twitter.com/ax___sh',
+    Website: 'https://ax-sh.github.io/portfolio/',
+    // CV: "",
+  },
+};
 
-export function Common(props: CommonProps) {
-  return (
-    <div>
-      <h1>Welcome to Common!</h1>
-    </div>
-  );
-}
+export const ExternalLinks = () => (
+  <div className="links">
+    <ul className="flex w-50 justify-around">
+      {Object.entries(data.links).map(([label, value]) => (
+        <li key={label}>
+          <Obfuscate target="_blank" href={value}>
+            <GetIcon name={label} />
+          </Obfuscate>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
 
-export default Common;
