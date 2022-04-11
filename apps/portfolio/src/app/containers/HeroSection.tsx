12ba67ec2a1098ddd44e670fc3430536c2c940aa
Axm <56112475+ax-sh@users.noreply.github.com>
2022-04-11 22:09:42 +0545

    feat: update wrapper

--- apps/portfolio/src/app/containers/HeroSection.tsx
+++ apps/portfolio/src/app/containers/HeroSection.tsx
@@ -9,7 +9,6 @@ import {
   Graphql,
   Java,
   Javascript,
-
   Jira,
   Nestjs,
   Netlify,
@@ -22,62 +21,66 @@ import {
   ReactJs,
   Redux,
   Reduxsaga,
- 
   Tsnode,
   Typescript,
 } from '@icons-pack/react-simple-icons';
 
-
-export interface IconsWrapperProps extends React.HTMLAttributes<HTMLDivElement> {
+export interface IconsWrapperProps
+  extends React.HTMLAttributes<HTMLDivElement> {
   size: number;
 }
 
-function IconsWrapper({children, size, ...props }: IconsWrapperProps) {
-  return <div {...props}>
-    {React.Children.map(children, (child: any) => React.cloneElement(child, {size})
-    )}
-  </div>;
+function IconsWrapper({ children, size, ...props }: IconsWrapperProps) {
+  return (
+    <div {...props}>
+      {React.Children.map(children, (child: any) =>
+        React.cloneElement(child, { size })
+      )}
+    </div>
+  );
 }
 
 export const HeroSection = () => {
   return (
     <section
       className="h-screen grid place-items-center bg-cover bg-center"
-      style={{backgroundImage: "url('./bg.jpg')"}}
+      style={{ backgroundImage: "url('./bg.jpg')" }}
     >
       <div>
         <h1 className="text-8xl">AX-SH</h1>
-        <h2 className="text-4xl">Languages</h2>
+        <div className={"bg-black bg-opacity-50 p-2 rounded-md flex flex-col gap-6"}>
+
+
+        <h2 className="text-4xl">Programming Languages</h2>
         <div className={'flex justify-items-center flex-wrap gap-2'}>
-          <IconsWrapper size={48} className={"flex"}>
+          <IconsWrapper size={48} className={'flex gap-4 '}>
             <Javascript />
-            <Typescript/>
+            <Typescript />
             <Nodedotjs />
             <Tsnode />
             <Python />
             <Java />
-            <Go/>
+            <Go />
             <Gnubash />
           </IconsWrapper>
-
-        </div>
-        <h2 className="text-4xl">Current Stacks I use</h2>
+        </div>     </div>
+        <h2 className="text-4xl">My Current Stacks</h2>
         <div className={'flex gap-2'}>
-          <Nx/>
-          <Github/>
-          <Gitlab/>
-          <Netlify/>
-          <Notion/>
-          <Git/>
-          <ReactJs/>
-          <Nextdotjs/>
-          <Nestjs/>
-          <Apollographql/>
-          <Graphql/>
-          <Redux/>
-          <Reduxsaga/>
-          <Nodemon/>
-          <Jira/>
+          <Nx />
+          <Github />
+          <Gitlab />
+          <Netlify />
+          <Notion />
+          <Git />
+          <ReactJs />
+          <Nextdotjs />
+          <Nestjs />
+          <Apollographql />
+          <Graphql />
+          <Redux />
+          <Reduxsaga />
+          <Nodemon />
+          <Jira />
         </div>
       </div>
     </section>
