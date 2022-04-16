ff2b7e5471534e5e285470330e728478b3cdc75e
Axm <56112475+ax-sh@users.noreply.github.com>
2022-04-16 22:38:24 +0545

    feat: add pages to vite config

--- apps/portfolio/vite.config.ts
+++ apps/portfolio/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 // import react from '@vitejs/plugin-react';
 import WindiCSS from 'vite-plugin-windicss';
+import Pages from 'vite-plugin-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [WindiCSS()],
+  plugins: [WindiCSS(), Pages()],
   // plugins: [react(), WindiCSS()],
   server: { host: true },
 });
