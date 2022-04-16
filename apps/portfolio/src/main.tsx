85834dcb5eeb6d6523af6c53c291b0ba54b423a3
Axm <56112475+ax-sh@users.noreply.github.com>
2022-04-16 22:37:59 +0545

    feat: update deps

--- apps/portfolio/src/main.tsx
+++ apps/portfolio/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import 'virtual:windi.css';
 import 'virtual:windi-devtools';
 
