61a1e006704249391541626c7333e6730166ca71
Axm <56112475+ax-sh@users.noreply.github.com>
2022-04-16 22:29:08 +0545

    refactor: components

--- apps/portfolio/src/app/containers/WineHeroSection.tsx
+++ apps/portfolio/src/app/containers/WineHeroSection.tsx
@@ -29,6 +29,60 @@ function NavBar() {
   );
 }
 
+function MiddleGrid() {
+  return (
+    <div className={'h-full w-full'}>
+      <video
+        src={'/wine.mp4'}
+        muted
+        autoPlay
+        loop
+        className={'block h-full w-full object-contain m-auto'}
+      />
+    </div>
+  );
+}
+
+function ScrollDownSection() {
+  return (
+    <div className={'absolute bottom-0 w-full text-center p-4'}>
+      <span className={'text-xs bg-black pb-2 border-b border-red-600'}>
+        SCROLL DOWN
+      </span>
+    </div>
+  );
+}
+
+function RightGrid() {
+  return (
+    <div className={'grid grid-rows-2'}>
+      <div className={'row-start-2 row-span-1 p-10 flex flex-col gap-10'}>
+        <div className={'h-full w-1/2 bg-white text-black p-5 rounded-md'}>
+          The Story Of Our Wines
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function OurWinesButton() {
+  return (
+    <button className={'bg-red-600 px-20 py-5 rounded-sm'}>Our Wines</button>
+  );
+}
+
+function LeftGrid() {
+  return (
+    <div className={'grid grid-rows-2'}>
+      <div className={'row-start-3 p-10 flex flex-col gap-10'}>
+        <h5 className={'text-xs uppercase'}>Italian Wine since 1555</h5>
+        <h4 className={'text-5xl'}>The Great Italian Wines With A History</h4>
+        <OurWinesButton />
+      </div>
+    </div>
+  );
+}
+
 export function WineHeroSection() {
   return (
     <>
@@ -36,41 +90,11 @@ export function WineHeroSection() {
       <section className={'min-h-screen md:h-screen relative flex flex-col'}>
         <div className={'h-30 md:h-0'}></div>
         <div className={'flex-grow md:grid grid-cols-3 grid-rows-1 h-full'}>
-          <div className={'grid grid-rows-2'}>
-            <div className={'row-start-3 p-10 flex flex-col gap-10'}>
-              <h5 className={'text-xs uppercase'}>Italian Wine since 1555</h5>
-              <h4 className={'text-5xl'}>
-                The Great Italian Wines With A History
-              </h4>
-              <button className={'bg-red-600 px-20 py-5 rounded-sm'}>
-                Our Wines
-              </button>
-            </div>
-          </div>
-
-          <div className={'h-full w-full'}>
-            <video
-              src={'/wine.mp4'}
-              muted
-              autoPlay
-              loop
-              className={'block h-full w-full object-cover m-auto'}
-            />
-          </div>
-
-          <div className={'grid grid-rows-2'}>
-            <div className={'row-start-2 row-span-1 p-10 flex flex-col gap-10'}>
-              <div
-                className={'h-full w-1/2 bg-white text-black p-5 rounded-md'}
-              >
-                The Story Of Our Wines
-              </div>
-            </div>
-          </div>
+          <LeftGrid />
+          <MiddleGrid />
+          <RightGrid />
         </div>
-        <div className={'absolute bottom-0 w-full text-center p-4'}>
-          <span className={'text-xs'}>SCROLL DOWN</span>
-        </div>
+        <ScrollDownSection />
       </section>
     </>
   );
