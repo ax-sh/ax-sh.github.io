bea26d0fb0a1dcc349211d3a6e2850a39178b6e0
Axm <56112475+ax-sh@users.noreply.github.com>
2022-04-17 22:16:25 +0545

    Updates

--- assets/_...all_.9152a81d.js
+++
@@ -1,1 +0,0 @@
-import{W as s,j as e}from"./vendor.9e57b029.js";const a=()=>{const o=s();return console.log(o),e("div",{children:"nop"})};export{a as default};
---
+++ assets/_...all_.9fd8fb5b.js
@@ -0,0 +1,1 @@
+import{W as e,j as o}from"./vendor.9e57b029.js";const a=()=>{const s=e();return console.log(s),o("div",{children:"404"})};export{a as default};
