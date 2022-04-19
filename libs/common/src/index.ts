7fd815bb4209088f179b650d848024dc3d5429ce
Axm <56112475+ax-sh@users.noreply.github.com>
2022-04-19 13:32:25 +0545

    feat: add GenderToggleButton to libs

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -1,3 +1,3 @@
 export * from './lib/common';
-export * from './lib/Icons'
-export * from './lib/GenderToggleButton/GenderToggleButton'
+export * from './lib/Icons';
+export * from './lib/GenderToggleButton/GenderToggleButton';
