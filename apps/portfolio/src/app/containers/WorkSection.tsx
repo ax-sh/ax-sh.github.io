736a7add5d49098e169c8905ed1753eba2c3f997
Axm <56112475+ax-sh@users.noreply.github.com>
2022-04-19 13:48:59 +0545

    feat: add component

--- apps/portfolio/src/app/containers/WorkSection.tsx
+++ apps/portfolio/src/app/containers/WorkSection.tsx
@@ -1,5 +1,10 @@
-import React from "react";
+import React from 'react';
+import { GenderToggleButton } from '@ax-sh.github.io/common';
 
 export function WorkSection() {
-  return <section></section>;
+  return (
+    <section className={'h-screen grid place-items-center'}>
+      <GenderToggleButton />
+    </section>
+  );
 }
