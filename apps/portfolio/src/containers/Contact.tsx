e4c269ca46971d229534a605157ff4823bd97ce9
Axm <56112475+ax-sh@users.noreply.github.com>
2022-08-04 22:14:35 +0545

    feat: add react hook forms contact section

--- apps/portfolio/src/containers/Contact.tsx
+++ apps/portfolio/src/containers/Contact.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
-import {Form} from "@ax-sh.github.io/common";
+import { Form } from '@ax-sh.github.io/common';
 
 export function Contact() {
-  function handleSubmit(data:any) {
-    console.log(data)
+  function handleSubmit(data: any) {
+    console.log(data);
   }
 
   return (
-    <section id={"contact"} className={'h-screen grid place-items-center'}>
-
-      <Form onSubmit={handleSubmit}>
-
-      </Form>
+    <section id={'contact'} className={'h-screen grid place-items-center'}>
+      <Form onSubmit={handleSubmit}></Form>
     </section>
   );
 }
