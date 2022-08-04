3ae4e44dd7c5c25b115c7d77c497b30a1d3e4bda
Axm <56112475+ax-sh@users.noreply.github.com>
2022-08-04 22:50:36 +0545

    feat: add qrcode and other helper on server start

--- apps/portfolio/vite.config.ts
+++ apps/portfolio/vite.config.ts
@@ -1,13 +1,20 @@
 import { defineConfig } from 'vite';
 import viteCompression from 'vite-plugin-compression';
-
+import { qrcode } from 'vite-plugin-qrcode';
+import svgr from 'vite-plugin-svgr'
 // import react from '@vitejs/plugin-react';
 import WindiCSS from 'vite-plugin-windicss';
 import Pages from 'vite-plugin-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [WindiCSS(), Pages(),viteCompression({algorithm:"brotliCompress"})],
+  plugins: [
+    WindiCSS(),
+    Pages(),
+    svgr(),
+    qrcode() ,// only applies in dev mode
+    viteCompression({ algorithm: 'brotliCompress' }),
+  ],
   // plugins: [react(), WindiCSS()],
   server: { host: true },
 });
