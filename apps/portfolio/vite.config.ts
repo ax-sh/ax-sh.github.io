11b754e9ea28673a26e33a99702b334972f5e857
Axm <56112475+ax-sh@users.noreply.github.com>
2022-08-04 22:39:18 +0545

    feat: add meta tag

--- apps/portfolio/vite.config.ts
+++ apps/portfolio/vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
+import viteCompression from 'vite-plugin-compression';
+
 // import react from '@vitejs/plugin-react';
 import WindiCSS from 'vite-plugin-windicss';
 import Pages from 'vite-plugin-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [WindiCSS(), Pages()],
+  plugins: [WindiCSS(), Pages(),viteCompression({algorithm:"brotliCompress"})],
   // plugins: [react(), WindiCSS()],
   server: { host: true },
 });
