6d0e92d7024096c52f6679e81ca66cfd67fbe25a
Axm <56112475+ax-sh@users.noreply.github.com>
2022-08-04 18:58:55 +0545

    feat: update description

--- apps/portfolio/src/app/components/Nav.tsx
+++ apps/portfolio/src/app/components/Nav.tsx
@@ -2,20 +2,19 @@ import React from 'react';
 import { List } from './List';
 
 export default function Nav() {
-  const [items] = React.useState(['HOME', 'CONTACT', 'ABOUT']);
+  const [items] = React.useState(['SKILLS', 'CONTACT', 'ABOUT']);
   return (
     <nav className="fixed w-full">
-      <div className={"container flex justify-between py-10"}>
-
-      <div>Axmin Shrestha</div>
-      <List className="flex w-56 justify-between ">
-        {items.map((item) => (
-          <a className="hover:text-red-500" href="#home">
-            {item}
-          </a>
-        ))}
-      </List>
-    </div>
+      <div className={'container flex justify-between py-10'}>
+        <div>Axmin Shrestha</div>
+        <List className="flex w-56 justify-between ">
+          {items.map((item) => (
+            <a className="hover:text-red-500 cursor-pointer" >
+              {item}
+            </a>
+          ))}
+        </List>
+      </div>
     </nav>
   );
 }
--- apps/portfolio/src/pages/index.tsx
+++ apps/portfolio/src/pages/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { ExternalLinks } from '@ax-sh.github.io/common';
 // import { WorkSection } from '../app/containers/WorkSection';
 import Nav from '../app/components/Nav';
-import clsx from "clsx";
+import clsx from 'clsx';
 
 const Layout = ({
   children,
@@ -25,16 +25,27 @@ const Layout = ({
 };
 
 function Hero() {
-  return <section className={clsx("container grid grid-cols-2 pt-30")}>
-    <div className={"flex flex-col gap-3"}>
-      <h1 className={"text-9xl"}><span>Axmin</span>  Shrestha</h1>
-      <strong className={"text-xl"}>// About Me</strong>
-      <h2 className={"text-3xl"}>Experienced Web Developer remotely from Nepal</h2>
-      <p>Im a Creative Web Developer, Expert in responsive design, aesthetics, simplicity, and utility.
-        Interested in making things that people want, need, or desire.</p>
-    </div>
-    <div></div>
-  </section>;
+  return (
+    <section className={clsx('container grid grid-span-1 md:grid-cols-2 ')}>
+      <div className={'flex flex-col gap-3 col-span-1 pt-30'}>
+        <h1 className={'text-9xl'}>
+          Axmin <span className={"text-8xl"}>Shrestha</span>
+        </h1>
+        <strong className={'text-xl'}>// About Me</strong>
+        <h2 className={'text-3xl'}>
+          Experienced Web Developer remotely from Nepal
+        </h2>
+        <p>
+          Im a Creative Web Developer. Expert in Responsive design, aesthetics,
+          simplicity, and utility. Interested in making things that people want,
+          need, or desire.
+        </p>
+      </div>
+      <div className={"col-span-1 bg-[#111] pt-30"}>
+        <button className={"h-30 w-30 rounded-full bg-white text-black text-xl font-bold transform -translate-x-1/2"}>Hire Me</button>
+      </div>
+    </section>
+  );
 }
 
 function App() {
