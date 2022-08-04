31b9f54e9ba5c0ad45541c5c2e7fad6ba036ac5e
Axm <56112475+ax-sh@users.noreply.github.com>
2022-08-04 19:05:36 +0545

    fix: navbar

--- apps/portfolio/src/app/components/Nav.tsx
+++ apps/portfolio/src/app/components/Nav.tsx
@@ -7,7 +7,7 @@ export default function Nav() {
     <nav className="fixed w-full">
       <div className={'container flex justify-between py-10'}>
         <div>Axmin Shrestha</div>
-        <List className="flex w-56 justify-between ">
+        <List className="flex gap-3 justify-between p-4">
           {items.map((item) => (
             <a className="hover:text-red-500 cursor-pointer">{item}</a>
           ))}
--- apps/portfolio/src/pages/index.tsx
+++ apps/portfolio/src/pages/index.tsx
@@ -27,36 +27,33 @@ const Layout = ({
 function Hero() {
   return (
     <section className={clsx('container')}>
-      <div className={" grid grid-span-1 md:grid-cols-2 "}>
-
-
-      <div className={'flex flex-col gap-3 col-span-1 pt-30'}>
-        <h1 className={'text-9xl'}>
-          Axmin <span className={'text-8xl'}>Shrestha</span>
-        </h1>
-        <strong className={'text-xl'}>// About Me</strong>
-        <h2 className={'text-3xl'}>
-          Experienced Web Developer remotely from Nepal
-        </h2>
-        <p>
-          Im a Creative Web Developer. Expert in Responsive design, aesthetics,
-          simplicity, and utility. Interested in making things that people want,
-          need, or desire.
-        </p>
-      </div>
-      <div className={'col-span-1 bg-[#111] pt-30 order-first md:order-none'}>
-        <button
-          className={
-           clsx( 'hidden md:block md:h-30 md:w-30 w-full md:rounded-full bg-white text-black',' text-xl font-bold transform md:-translate-x-1/2')
-          }
-        >
-          Hire Me
-        </button>
-      </div>
-
-      </div>
-      <div className={"skills"}>
+      <div className={' grid grid-span-1 md:grid-cols-2 '}>
+        <div className={'flex flex-col gap-3 col-span-1 pt-30'}>
+          <h1 className={'text-9xl'}>
+            Axmin <span className={'text-8xl'}>Shrestha</span>
+          </h1>
+          <strong className={'text-xl'}>// About Me</strong>
+          <h2 className={'text-3xl'}>
+            Experienced Web Developer remotely from Nepal
+          </h2>
+          <p>
+            Im a Creative Web Developer. Expert in Responsive design,
+            aesthetics, simplicity, and utility. Interested in making things
+            that people want, need, or desire.
+          </p>
         </div>
+        <div className={'col-span-1 bg-[#111] pt-30 order-first md:order-none'}>
+          <button
+            className={clsx(
+              'hidden md:block md:h-30 md:w-30 w-full md:rounded-full bg-white text-black',
+              ' text-xl font-bold transform md:-translate-x-1/2'
+            )}
+          >
+            Hire Me
+          </button>
+        </div>
+      </div>
+      <div className={'skills'}></div>
     </section>
   );
 }
