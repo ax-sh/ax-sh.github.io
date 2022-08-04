85683aafc99a19c44a4ce05bb52fb2bec92a4837
Axm <56112475+ax-sh@users.noreply.github.com>
2022-08-04 22:00:43 +0545

    refactor: fix colors

--- apps/portfolio/src/components/Nav.tsx
+++ apps/portfolio/src/components/Nav.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { List } from './List';
-import {Link} from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 export default function Nav() {
   const [items] = React.useState(['SKILLS', 'CONTACT', 'ABOUT']);
@@ -8,12 +8,11 @@ export default function Nav() {
     <nav className="fixed w-full">
       <div className={'container flex justify-between items-center py-10'}>
         <Link to={'/'}>
-
-
-        <div className={"text-xl"}>
-          <span className={'text-[#F71735]'}>Ax</span>min{' '}
-          <span className={'text-[#03F7EB]'}>Sh</span>restha
-        </div>  </Link>
+          <div className={'text-xl'}>
+            <span className={'text-[#F71735]'}>Ax</span>min{' '}
+            <span className={'text-[#03F7EB]'}>Sh</span>restha
+          </div>{' '}
+        </Link>
         <List className="flex gap-3 justify-between p-4">
           {items.map((item) => (
             <a className="hover:text-red-500 cursor-pointer">{item}</a>
