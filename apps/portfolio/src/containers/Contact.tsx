af923f5429c7dc6ed8c52fe081882ff44dff726d
Axm <56112475+ax-sh@users.noreply.github.com>
2022-08-06 01:22:44 +0545

    feat: add props

--- apps/portfolio/src/containers/Contact.tsx
+++ apps/portfolio/src/containers/Contact.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
 import { Form } from '@ax-sh.github.io/common';
 
+type Data = {};
+
 export function Contact() {
-  function handleSubmit(data: any) {
+  function handleSubmit(data: Data) {
     console.log(data);
   }
 
   return (
     <section id={'contact'} className={'h-screen grid place-items-center'}>
-      <Form onSubmit={handleSubmit}></Form>
+      <Form<Data> onSubmit={handleSubmit}></Form>
     </section>
   );
 }
