43ed0f9605d0776c9c594e6bef0343aa284cf033
Axm <56112475+ax-sh@users.noreply.github.com>
2022-08-06 12:59:01 +0545

    feat: add layout

--- apps/portfolio-next/pages/index.tsx
+++ apps/portfolio-next/pages/index.tsx
@@ -1,7 +1,33 @@
 import Layout from '../components/layout';
 
+function Hero() {
+	return (
+		<section>
+			<h1>Axmin Shrestha</h1>
+		</section>
+	);
+}
+function About() {
+	return (
+		<section id="about" className="">
+			<h2>{"I'm Axmin Shrestha"}</h2>
+			<p></p>
+			{/* experience */}
+		</section>
+	);
+}
+
 export function Index() {
-	return <Layout>Axmin Shrestha</Layout>;
+	return (
+		<Layout>
+			<Hero />
+			<About />
+			{/* work
+			project 
+			contact 
+			*/}
+		</Layout>
+	);
 }
 
 export default Index;
