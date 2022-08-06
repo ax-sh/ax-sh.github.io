4a8f9a2f4601b5bb584b49f6b685c5ebb0743eb9
Axm <56112475+ax-sh@users.noreply.github.com>
2022-08-06 12:59:01 +0545

    feat: add layout

--- apps/portfolio-next/components/layout.tsx
+++ apps/portfolio-next/components/layout.tsx
@@ -1,15 +1,18 @@
 // import Navbar from './navbar';
 // import Footer from './footer';
 
+import clsx from 'clsx';
 import React from 'react';
 
 type LayoutProps = React.ComponentPropsWithoutRef<'main'>;
 
-export default function Layout({ children }: LayoutProps) {
+export default function Layout({ children, className, ...props }: LayoutProps) {
 	return (
 		<>
 			{/* <Navbar /> */}
-			<main>{children}</main>
+			<main className={clsx('main', className)} {...props}>
+				{children}
+			</main>
 			{/* <Footer /> */}
 		</>
 	);
