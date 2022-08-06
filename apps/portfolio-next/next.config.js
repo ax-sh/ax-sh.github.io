dd3ace335dcfba37c85588496b804c54ab2a22f5
Axm <56112475+ax-sh@users.noreply.github.com>
2022-08-06 13:39:36 +0545

    feat: add pwa

--- apps/portfolio-next/next.config.js
+++ apps/portfolio-next/next.config.js
@@ -1,15 +1,18 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withNx = require('@nrwl/next/plugins/with-nx');
+const withPWA = require('next-pwa');
 
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
-const nextConfig = {
-  nx: {
-    // Set this to true if you would like to to use SVGR
-    // See: https://github.com/gregberge/svgr
-    svgr: false,
-  },
-};
+const nextConfig = withPWA({
+	swcMinify: true,
+	nx: {
+		// Set this to true if you would like to to use SVGR
+		// See: https://github.com/gregberge/svgr
+		svgr: true,
+	},
+	pwa: { dest: 'public' },
+});
 
 module.exports = withNx(nextConfig);
