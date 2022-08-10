d682487cb26f0b0ff2c2ab9c2954883b37708a94
Axm <56112475+ax-sh@users.noreply.github.com>
2022-08-11 02:05:57 +0545

    feat: wip

--- apps/portfolio-next/next.config.js
+++ apps/portfolio-next/next.config.js
@@ -1,6 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withNx = require('@nrwl/next/plugins/with-nx');
 const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
 
 const isProduction = process.env.NODE_ENV === 'production';
 
@@ -9,12 +10,15 @@ const isProduction = process.env.NODE_ENV === 'production';
  **/
 const nextConfig = withPWA({
 	swcMinify: true,
+	compiler: {
+		styledComponents: true,
+	},
 	nx: {
 		// Set this to true if you would like to to use SVGR
 		// See: https://github.com/gregberge/svgr
 		svgr: true,
 	},
-	pwa: { disable: !isProduction, dest: 'public' },
+	pwa: { disable: !isProduction, dest: 'public', runtimeCaching },
 	i18n: {
 		locales: ['en'],
 		defaultLocale: 'en',
