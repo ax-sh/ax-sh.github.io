5d3f7b46584b79165985dcdf4903607214bf5ed8
Axm <56112475+ax-sh@users.noreply.github.com>
2022-08-11 11:09:33 +0545

    feat: update seo

--- apps/portfolio-next/next-seo.config.ts
+++ apps/portfolio-next/next-seo.config.ts
@@ -4,11 +4,13 @@ const SEOConfig = {
 		locale: 'en_IE',
 		url: 'https://ax-sh.netlify.app/',
 		site_name: 'Axmin Shrestha',
+		title: 'Axmin Shrestha Portfolio',
 	},
 	twitter: {
 		handle: '@ax___sh',
-		site: '@site',
+		site: '@ax___sh',
 		cardType: 'summary_large_image',
+		title: 'Axmin Shrestha',
 	},
 };
 export default SEOConfig;
