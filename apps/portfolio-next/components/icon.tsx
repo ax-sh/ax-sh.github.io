458f45ab87343748ba6707a4fa56de0f95cbb5d7
Axm <56112475+ax-sh@users.noreply.github.com>
2022-08-12 08:26:23 +0545

    refactor: icon logic

--- apps/portfolio-next/components/icon.tsx
+++ apps/portfolio-next/components/icon.tsx
@@ -19,38 +19,31 @@ enum IconNames {
 type IconProps = { name?: keyof typeof IconNames };
 
 function getIcon(name: keyof typeof IconNames) {
-	return;
+	switch (name) {
+		case 'react_query':
+			return siReactquery;
+		case 'rhf':
+			return siReacthookform;
+		case 'three':
+			return siThreedotjs;
+		case 'react':
+			return siReact;
+		case 'next':
+			return siNextdotjs;
+		case 'typescript':
+			return siTypescript;
+		default:
+			return name;
+	}
 }
 const Icon = React.forwardRef<SVGElement, Omit<SVGProps, 'src'> & IconProps>(
 	({ name, ...props }, ref) => {
 		props.width = 62;
+		const icon = getIcon(name);
 
-		console.log(siTypescript);
+		console.log(icon);
 
-		switch (name) {
-			case 'react_query':
-				return <SVG innerRef={ref} src={siReactquery.svg} {...props} />;
-			case 'rhf':
-				return <SVG innerRef={ref} src={siReacthookform.svg} {...props} />;
-			case 'typescript':
-				return (
-					<SVG
-						innerRef={ref}
-						src={siTypescript.svg}
-						color={`#${siTypescript.hex}`}
-						fill={`#${siTypescript.hex}`}
-						{...props}
-					/>
-				);
-			case 'three':
-				return <SVG innerRef={ref} src={siThreedotjs.svg} {...props} />;
-			case 'react':
-				return <SVG innerRef={ref} src={siNextdotjs.svg} {...props} />;
-			case 'next':
-				return <SVG innerRef={ref} src={siReact.svg} {...props} />;
-			default:
-				return name;
-		}
+		return <SVG innerRef={ref} src={icon.svg} fill={`#${icon.hex}`} {...props} />;
 	}
 );
 Icon.displayName = 'Icon';
