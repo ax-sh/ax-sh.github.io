a8330527973d4c794851ea1a3f711880b9d759b9
Axm <56112475+ax-sh@users.noreply.github.com>
2022-08-14 12:28:47 +0545

    fix: responsiveness of project section

--- apps/portfolio/src/containers/Projects.tsx
+++ apps/portfolio/src/containers/Projects.tsx
@@ -27,21 +27,28 @@ const projects: ProjectProps[] = [
 ];
 function Project({ link, name, external, image }: ProjectProps) {
 	return (
-		<div className="project hover:opacity-60 h-42 relative">
-			<a className="cursor-pointer text-3xl" target="_blank" rel="noreferrer" href={link}>
-				{external && (
-					<span className="absolute right-2 top-2">
-						<TbExternalLink />
-					</span>
-				)}
-				<img
-					className="h-full w-full aspect-video	object-cover"
-					src={image ? image : 'https://source.unsplash.com/ZInLhWJCZTI'}
-					alt="project"
-				/>
+		<a
+			className="cursor-pointer text-3xl hover:opacity-60 "
+			target="_blank"
+			rel="noreferrer"
+			href={link}
+		>
+			<div className="project  flex flex-col">
+				<div className="flex-grow relative overflow-hidden h-42">
+					{external && (
+						<span className="absolute right-2 top-2">
+							<TbExternalLink />
+						</span>
+					)}
+					<img
+						className="h-full w-full aspect-video object-cover"
+						src={image ? image : 'https://source.unsplash.com/ZInLhWJCZTI'}
+						alt="project"
+					/>
+				</div>
 				<h3 className="py-3">{name}</h3>
-			</a>
-		</div>
+			</div>
+		</a>
 	);
 }
 
@@ -49,7 +56,7 @@ export default function Projects() {
 	return (
 		<section className={'container min-h-screen py-9'}>
 			<h2 className="text-5xl py-9">Projects</h2>
-			<div className="grid grid-cols-3 gap-3">
+			<div className="grid grid-cols-1 md:grid-cols-3 gap-3">
 				{projects.map((i: ProjectProps, index) => (
 					<React.Fragment key={index}>
 						<Project {...i} />
