8aa68c4a4d24bb60bb8eaa9114cdf71f83dcc693
Axm <56112475+ax-sh@users.noreply.github.com>
2022-08-16 08:50:48 +0545

    feat: add timeline

--- apps/portfolio-next/components/hero.tsx
+++ apps/portfolio-next/components/hero.tsx
@@ -1,23 +1,39 @@
 import React from 'react';
 import clsx from 'clsx';
 import gsap from 'gsap';
-
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+import { useIsomorphicLayoutEffect } from '@ax-sh.github.io/common';
+gsap.registerPlugin(ScrollTrigger);
 type HeroProps = React.ComponentPropsWithoutRef<'section'>;
 
-export default function Hero({ className, ...props }: HeroProps) {
-	const tween = React.useRef(gsap.timeline({ paused: true }));
-	const ref = React.useRef();
+function getTimeline() {
+	return gsap.timeline({
+		paused: true,
+		scrollTrigger: {
+			trigger: '.hero',
+			start: 'top bottom',
+			end: 'bottom +=20%',
+			markers: true,
+			invalidateOnRefresh: true,
+			scrub: true,
+		},
+	});
+}
 
-	React.useEffect(() => {
-		tween.current.to(ref.current, { duration: 1, x: 100, y: 100 });
+export default function Hero({ className, ...props }: HeroProps) {
+	const tween = React.useRef<gsap.core.Timeline>(getTimeline());
+	const ref = React.useRef<HTMLDivElement>();
+
+	useIsomorphicLayoutEffect(() => {
+		tween.current.to(ref.current, { duration: 1, xPercent: 100, y: 100 });
 
 		return () => {
 			tween.current.kill();
 		};
 	}, []);
 	return (
-		<section className={clsx('hero', className)} {...props}>
-			uoooo
+		<section className={clsx('hero overflow-hidden', className)} {...props}>
+			<div ref={ref}>GSAP</div>
 		</section>
 	);
 }
