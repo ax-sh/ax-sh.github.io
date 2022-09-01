503ffd44a93014959388a757cea5dd003e361188
Axm <56112475+ax-sh@users.noreply.github.com>
2022-09-01 09:37:02 +0545

    feat: update seo

--- apps/portfolio-next/next-seo.config.ts
+++ apps/portfolio-next/next-seo.config.ts
@@ -2,14 +2,15 @@ const title = 'Axmin Shrestha';
 
 const SEOConfig = {
 	title,
-	description: 'Axmin Shrestha(ax-sh) Portfolio',
+	description:
+		'Axmin Shrestha(ax-sh) is a Full-Stack Developer. Specializes in Conceptual and Interactive UI Design. Focuses on designing work based on the aesthetics, usability and simplicity.',
 	canonical: 'https://ax-sh.netlify.app/',
 	openGraph: {
 		type: 'website',
 		locale: 'en_IE',
 		url: 'https://ax-sh.netlify.app/',
 		site_name: 'Axmin Shrestha',
-		title: 'Axmin Shrestha Portfolio',
+		title: 'Axmin Shrestha Full Stack Portfolio',
 	},
 	twitter: {
 		handle: '@ax___sh',
