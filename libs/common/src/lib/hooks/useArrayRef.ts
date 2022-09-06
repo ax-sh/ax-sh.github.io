05f825474f7a0106a6af38b482898909c109bdd6
Axm <56112475+ax-sh@users.noreply.github.com>
2022-09-06 17:25:33 +0545

    fix: type

--- libs/common/src/lib/hooks/useArrayRef.ts
+++ libs/common/src/lib/hooks/useArrayRef.ts
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export function useArrayRef<T = unknown>(): [React.MutableRefObject<T[]>, (ref: T) => void] {
+export function useArrayRef<T = HTMLElement>(): [React.MutableRefObject<T[]>, (ref: T) => void] {
 	const refs = React.useRef<T[]>([]);
 	refs.current = [];
 	return [refs, (ref: T) => ref && refs.current.push(ref)];
