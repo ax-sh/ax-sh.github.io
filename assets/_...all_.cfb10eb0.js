353b9647b68059ce9337a8998cf18aeb6f325b18
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-08-11 01:49:43 +0545

    Updates

--- assets/_...all_.cfb10eb0.js
+++
@@ -1,1 +0,0 @@
-import{u as o,j as a}from"./index.dcc9be96.js";const r=()=>{const s=o();return console.log(s),a("div",{children:"404"})};export{r as default};
---
+++ assets/_...all_.fbc1b762.js
@@ -0,0 +1,1 @@
+import{u as o,j as a}from"./index.4348489d.js";const r=()=>{const s=o();return console.log(s),a("div",{children:"404"})};export{r as default};
