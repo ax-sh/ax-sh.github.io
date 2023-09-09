1fc606d6993cf1577e5363df1d98a30392e2d273
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-09-09 18:42:56 +0545

    feat: add code quality tools

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,50 +1,46 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-	},
-	extends: [
-		"next",
-		"next/core-web-vitals",
-		'xo',
-		'airbnb',
-		'airbnb-typescript',
-		'plugin:react/recommended',
-		'prettier',
-	],
-	 parserOptions: {
-		 ecmaVersion: 'latest',
-		 sourceType: 'module',
-	  project: './tsconfig.json'
-	 },
-	overrides: [
-		{
-			env: {
-				node: true,
-			},
-			files: [
-				'.eslintrc.{js,cjs}',
-			],
-			parserOptions: {
-				sourceType: 'script',
-			},
-		},
-		{
-			extends: [
-				'xo-typescript',
-			],
-			files: [
-				'*.ts',
-				'*.tsx',
-			],
-		},
-	],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "next",
+    "next/core-web-vitals",
+    "xo",
+    "airbnb",
+    "airbnb-typescript",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+    "prettier",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    // project: "./tsconfig.json",
+  },
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+    {
+      extends: ["xo-typescript"],
+      files: ["*.ts", "*.tsx", "*.js"],
+      rules: {
+        "@typescript-eslint/object-curly-spacing":"off",
+        "unicorn/prefer-module":"off"
+      },
+    },
+  ],
 
-	plugins: [
-		'react',
-	],
-	rules: {
-		'object-curly-spacing': ['off'],
-		'quotes': ['off'],
-	},
+  plugins: ["react"],
+  rules: {
+    "object-curly-spacing": ["off"],
+    quotes: ["off"],
+  },
 };
