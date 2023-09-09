804d946b7115c886b09acc649e0a99a616671f5f
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-09-09 19:55:27 +0545

    feat: add hero image

--- app/hero-section.tsx
+++ app/hero-section.tsx
@@ -3,6 +3,20 @@ import Image from 'next/image'
 
 export function HeroSection() {
   return (
+    <section className={clsx('flex min-h-screen', 'flex-col-reverse md:flex-row')}>
+      <div className={clsx('relative grow shrink-0 basis-1/2')}>
+        <Image objectPosition={'bottom'} fill={true} objectFit={'contain'} src={'/hero-image.svg'} alt={'hero'} />
+      </div>
+      <div className={clsx('flex flex-col w-full self-stretch basis-1/2', 'md:grow', 'prose prose-neutral dark:prose-invert')}>
+        <div className={'shrink items-center justify-center b g-red-400'}>
+          <h1>AX-SH</h1>
+        </div>
+      </div>
+    </section>
+  )
+}
+export function HeroSectionGrid() {
+  return (
     <section className={clsx('grid grid-rows-1 min-h-screen grid-cols-1', 'md:grid-cols-2')}>
       <div className={clsx('relative row-span-full col-span-full md:col-span-1')}>
         <Image objectPosition={'bottom'} fill={true} objectFit={'contain'} src={'/hero-image.svg'} alt={'hero'} />
