a29d976ba16bb50be072df17ff93612d1dabd8e6
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-09-09 16:15:01 +0545

    feat: add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'xo',
+		'airbnb',
+		'airbnb-typescript',
+		'plugin:react/recommended',
+		'prettier',
+	],
+	 parserOptions: {
+		 ecmaVersion: 'latest',
+		 sourceType: 'module',
+	  project: './tsconfig.json'
+	 },
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: [
+				'.eslintrc.{js,cjs}',
+			],
+			parserOptions: {
+				sourceType: 'script',
+			},
+		},
+		{
+			extends: [
+				'xo-typescript',
+			],
+			files: [
+				'*.ts',
+				'*.tsx',
+			],
+		},
+	],
+
+	plugins: [
+		'react',
+	],
+	rules: {
+	},
+};
