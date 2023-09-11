a73e4f38434999f454c430af6a28b0ffcdffb224
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-09-11 06:02:35 +0545

    feat: add timeline component

--- app/timeline-section.tsx
+++ app/timeline-section.tsx
@@ -52,34 +52,46 @@ export function TimelineSection() {
         <div className="min-h-screen flex items-center justify-center ">
           <ul className="flex flex-col md:grid grid-cols-9 mx-auto p-2">
             {range(100).map((i) => (
-              <TimelineItem c odd={i % 2 === 0}>
-                <article className="relative shadow-2xl max-w-lg w-full transform duration-500 hover:-translate-y-2 cursor-pointer rounded-md">
-                  <div className="flex absolute left-0 top-0 w-10 h-10 bg-orange-600 text-gray-100">
-                    <span className="mx-auto my-auto">
-                      {/*<img className="w-5 h-5" src="/assets/left-arrow.801ca37c.svg" />*/}
-                    </span>
-                  </div>
-                  <div
-                    className="bg-no-repeat bg-center overflow-hidden mt-10 min-h-96"
-                    // style="background-image: url('svgs/blob-blue.svg');"
-                  >
-                    <img
-                      className="mx-auto p-5"
-                      src="https://www.dropbox.com/s/1fav310i2eqkdz8/tool2.png?dl=1"
-                      alt=""
-                    />
-                  </div>
-                  <div className="p-8 mb-10 mt-2">
-                    <p className="text-xl text-gray-500">04.</p>
-                    <h2 className="text-3xl mt-2">Multipurpose Wooden Tool</h2>
+              <TimelineItem odd={i % 2 !== 0}>
+                <article className="relative shadow-2xl max-w-lg w-full transform duration-500 hover:-translate-y-2 cursor-pointer rounded-md overflow-hidden">
+                  <img
+                    className="mx-auto object-fill h-full w-full not-prose"
+                    src={`https://picsum.photos/600/300?random=${i}`}
+                    // src="https://www.dropbox.com/s/1fav310i2eqkdz8/tool2.png?dl=1"
+                    alt=""
+                  />
+                  <div className={'absolute w-full h-full inset-0 flex p-8'}>
+                    <h3 className={'absolute text-white right-10 bottom-0 text-3xl mix-blend-screen'}>Amalgum</h3>
                   </div>
                 </article>
-                {/*<h3 className="font-semibold text-xl mb-1 text-gray-900">Title 2</h3>*/}
-                {/*<p className="text-gray-600">*/}
-                {/*  Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore nesciunt maiores ipsa molestiae*/}
-                {/*  magnam libero dolorem explicabo. Nobis veniam incidunt at, alias ipsam est suscipit maxime sunt quia.*/}
-                {/*  Consectetur, aut.*/}
-                {/*</p>*/}
+                {/*<article className="relative shadow-2xl max-w-lg w-full transform duration-500 hover:-translate-y-2 cursor-pointer rounded-md">*/}
+                {/*  <div className="flex absolute left-0 top-0 w-10 h-10 bg-orange-600 text-gray-100">*/}
+                {/*    <span className="mx-auto my-auto">*/}
+                {/*      /!*<img className="w-5 h-5" src="/assets/left-arrow.801ca37c.svg" />*!/*/}
+                {/*    </span>*/}
+                {/*  </div>*/}
+                {/*  <div*/}
+                {/*    className="bg-no-repeat bg-center overflow-hidden mt-10 min-h-96"*/}
+                {/*    // style="background-image: url('svgs/blob-blue.svg');"*/}
+                {/*  >*/}
+                {/*    <img*/}
+                {/*      className="mx-auto p-5"*/}
+                {/*      src={`https://picsum.photos/600/400?random=${i}`}*/}
+                {/*      // src="https://www.dropbox.com/s/1fav310i2eqkdz8/tool2.png?dl=1"*/}
+                {/*      alt=""*/}
+                {/*    />*/}
+                {/*  </div>*/}
+                {/*  <div className="p-8 mb-10 mt-2">*/}
+                {/*    <h3 className="text-xl text-gray-500">0{i + 1}.</h3>*/}
+                {/*    <h2 className="text-3xl mt-2">Lorem ipsum dolor sit amet</h2>*/}
+                {/*  </div>*/}
+                {/*</article>*/}
+                {/*/!*<h3 className="font-semibold text-xl mb-1 text-gray-900">Title 2</h3>*!/*/}
+                {/*/!*<p className="text-gray-600">*!/*/}
+                {/*/!*  Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore nesciunt maiores ipsa molestiae*!/*/}
+                {/*/!*  magnam libero dolorem explicabo. Nobis veniam incidunt at, alias ipsam est suscipit maxime sunt quia.*!/*/}
+                {/*/!*  Consectetur, aut.*!/*/}
+                {/*/!*</p>*!/*/}
               </TimelineItem>
             ))}
           </ul>
