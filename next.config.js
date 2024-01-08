5e1f10af04cd0f0caf055c476e960db7b0e7c742
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-01-09 01:32:00 +0545

    rewrite 2024 (#1)
    * feat: add AnalyticsProvider.tsx
    
    * feat: add coming soon section
    
    * feat: update config
    
    * feat: update deploy script
    
    * feat: update deploy script
    
    * feat: update deploy script
    
    * feat: update deploy script
    
    * feat: update deploy script
    
    * feat: update deploy script

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  // Add basePath
+  basePath: '/ax-sh.github.io',
+  images: {
+    unoptimized: true
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
