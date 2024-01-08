fcccdd80dd265f5d8ea02d6274634fff8b48723c
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-01-09 01:17:28 +0545

    feat: update config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
 
-module.exports = nextConfig
+  // Add basePath
+  basePath: '/https://ax-sh.github.io'
+};
+
+module.exports = nextConfig;
