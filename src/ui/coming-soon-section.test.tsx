86079f7d3c8de049c587575967418c460a45a0dc
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-01-09 03:14:32 +0545

    feat: add vitest

---
+++ src/ui/coming-soon-section.test.tsx
@@ -0,0 +1,8 @@
+// import { describe, expect, it } from 'vitest';
+import { ComingSoonSection } from './coming-soon-section';
+
+describe(ComingSoonSection.name, () => {
+  it('should load', () => {
+    expect(1).toBe(1);
+  });
+});
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,17 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import AutoImport from 'unplugin-auto-import/vite';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    AutoImport({
+      imports: ['vitest'],
+      dts: true // generate TypeScript declaration
+    })
+  ],
   test: {
     environment: 'jsdom',
     globals: true
