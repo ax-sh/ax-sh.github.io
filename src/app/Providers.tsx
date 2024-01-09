9d4eb8bfdb01160756bafe45a5a1acefaeb7b5db
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-01-09 14:46:18 +0545

    feat: add hotjar dep

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,8 +1,12 @@
 'use client';
 import { GoogleAnalytics } from 'nextjs-google-analytics';
 import { GoogleFonts } from 'next-google-fonts';
+// import { hotjar } from 'react-hotjar';
 
 export default function Analytics({ measurementId }: { measurementId: string }) {
+  // useEffect(() => {
+  //   hotjar.initialize(HJID, HJSV);
+  // }, []);
   return <GoogleAnalytics gaMeasurementId={measurementId} trackPageViews />;
 }
 
