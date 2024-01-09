1348333b4ccb64237f86cee0d0c6c528fc5d329f
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-01-09 13:33:25 +0545

    feat: add @icons-pack/react-simple-icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,18 @@ import { ComingSoonSection } from '@/ui/coming-soon-section';
 import { HeroSection } from '@/ui';
 
 import { ExternalLinksSection } from '@/ui/external-links-section';
+import clsx from 'clsx';
 
 export default function Home() {
   return (
     <main className='min-h-screen w-full'>
       <HeroSection>
         <section
-          className={' box-border p-4 md:p-4  h-full w-full grid place-content-center prose'}
+          className={clsx(
+            'h-full w-full',
+            'box-border p-4 md:p-4',
+            'grid place-content-center prose'
+          )}
         >
           <ComingSoonSection />
           <ExternalLinksSection />
