c2606f7756444d735410e0595d2de4e2ab5de622
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-01-09 13:47:41 +0545

    feat: add @icons-pack/react-simple-icons

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,6 +1,13 @@
 'use client';
 import { GoogleAnalytics } from 'nextjs-google-analytics';
+import { GoogleFonts } from 'next-google-fonts';
 
 export default function Analytics({ measurementId }: { measurementId: string }) {
   return <GoogleAnalytics gaMeasurementId={measurementId} trackPageViews />;
+}
+
+export function FontProvider() {
+  return (
+    <GoogleFonts href='https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap' />
+  );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { PropsWithChildren } from 'react';
-import Analytics from '@/app/AnalyticsProvider';
+import Analytics, { FontProvider } from '@/app/Providers';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,6 +16,7 @@ const GOOGLE_ANALYTICS_ID = 'G-N6207193QM';
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang='en'>
+      <FontProvider />
       <Analytics measurementId={GOOGLE_ANALYTICS_ID} />
       <body className={inter.className}>{children}</body>
     </html>
