0e8dbfbbd11eb609041e074248e1faedfec27bb1
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-01-09 12:38:49 +0545

    fix: ts relative input

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,10 +3,11 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import AutoImport from 'unplugin-auto-import/vite';
-
+import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   plugins: [
     react(),
+    tsconfigPaths(),
     AutoImport({
       imports: ['vitest'],
       dts: true // generate TypeScript declaration
@@ -14,6 +15,7 @@ export default defineConfig({
   ],
   test: {
     environment: 'jsdom',
-    globals: true
+    globals: true,
+    include: ['**/*.test.{ts,tsx}']
   }
 });
