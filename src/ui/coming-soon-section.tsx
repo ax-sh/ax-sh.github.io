d8a01f0a9374fcec3c3e4810ba12c9a32584d994
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-01-10 18:45:56 +0545

    feat: feature/add-static-json (#12)

--- src/ui/coming-soon-section.tsx
+++ src/ui/coming-soon-section.tsx
@@ -1,3 +1,11 @@
+'use client';
+import { useQuery } from '@tanstack/react-query';
+
+function useData() {
+  return useQuery({ queryKey: ['data'], queryFn: () => fetch('/data.json').then((x) => x.json()) });
+}
 export function ComingSoonSection() {
+  const { data } = useData();
+  console.log(data, 'data');
   return <h1 className={'text-4xl text-white m-0 p-0'}>Coming Soon</h1>;
 }
