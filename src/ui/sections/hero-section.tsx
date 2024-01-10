0301b2a0be7aae4c49511f74f7c89955c6bebb59
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-01-11 02:25:53 +0545

    feat: add vitest react test

--- src/ui/sections/hero-section.tsx
+++ src/ui/sections/hero-section.tsx
@@ -7,7 +7,7 @@ import clsx from 'clsx';
 export function HeroSection({ children }: PropsWithChildren) {
   return (
     <section className={clsx('flex min-h-screen', 'flex-col-reverse md:flex-row duration-150')}>
-      <div className={clsx('relative grow shrink-0 basis-1/2 opacity-80')}>
+      <div className={clsx('relative grow shrink-0 basis-1/2')}>
         <ImageContain src='/hero-image.svg' alt='hero' />
       </div>
       <div className={clsx('flex flex-col w-full self-stretch basis-1/2', 'md:grow duration-150')}>
