83e218506511fdb0e5ed2389e89f3b9c53be6ffc
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-01-10 19:52:12 +0545

    feat: add feature/add-skeleton-loader

--- src/ui/coming-soon-section.tsx
+++ src/ui/coming-soon-section.tsx
@@ -1,11 +1,32 @@
 'use client';
 import { useQuery } from '@tanstack/react-query';
+import { PlaceholderSkeleton } from '@/ui/placeholder-skeleton';
 
 function useData() {
   return useQuery({ queryKey: ['data'], queryFn: () => fetch('/data.json').then((x) => x.json()) });
 }
+
 export function ComingSoonSection() {
-  const { data } = useData();
-  console.log(data, 'data');
-  return <h1 className={'text-4xl text-white m-0 p-0'}>Coming Soon</h1>;
+  const { data, isLoading } = useData();
+  if (isLoading)
+    return (
+      <div>
+        <h1 className={'text-4xl text-white m-0 p-0'}>
+          <PlaceholderSkeleton length={'Axmin Shrestha | Portfolio'} />
+        </h1>
+        <p>
+          <PlaceholderSkeleton
+            length={
+              'I Develop Creative Websites. Specialize In Aesthetics, Responsive Design, Simplicity, And Utility.'
+            }
+          />
+        </p>
+      </div>
+    );
+  return (
+    <div>
+      <h1 className={'text-4xl text-white m-0 p-0'}>{data.title}</h1>
+      <p>{data.description}</p>
+    </div>
+  );
 }
