a246f24d56c89a22c3a935bb23d32a8506475c9f
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-01-10 18:37:55 +0545

    feat: add-react-query dev tools

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { PropsWithChildren } from 'react';
-import Analytics, { FontProvider, Providers } from '@/app/Providers';
+import Analytics, { FontProvider, Providers } from '@/app/providers';
 
 const inter = Inter({ subsets: ['latin'] });
 
