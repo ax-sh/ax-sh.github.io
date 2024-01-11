700bbef4fca9fcfdefaad79e5e904defa36dd5b6
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-01-11 11:45:33 +0545

    feat: add release-it cmd

--- scripts/release.ts
+++ scripts/release.ts
@@ -35,9 +35,7 @@ const Release = {
   async getNextVersion() {
     const currentVersion = await this.getCurrentVersion();
     const releaseType = await this.getReleaseType();
-    const nextVersionCmd = await $({
-      stdio: 'inherit'
-    })`pnpm release-it ${releaseType} --ci --release-version --dry-run`;
+    const nextVersionCmd = await $`pnpm release-it ${releaseType} --ci --release-version --dry-run`;
     const nextVersion = nextVersionCmd;
     console.log(currentVersion, nextVersion.stdout);
 
