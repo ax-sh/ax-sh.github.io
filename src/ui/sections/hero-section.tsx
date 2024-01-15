42153cd21c1b1b6caaa35340936dc7432fd74ae3
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-01-15 05:46:19 +0545

    feat: add animated hero image

--- src/ui/sections/hero-section.tsx
+++ src/ui/sections/hero-section.tsx
@@ -4,13 +4,39 @@ import { ImageContain } from "@/ui";
 
 import clsx from "clsx";
 
+function HeroImage() {
+  return (
+    <>
+      <ImageContain src='/hero-image.svg' alt='hero' className='z-10' />
+      <ImageContain src='/hero-image.svg' alt='shadow' className='card-fader' />
+      <ImageContain src='/hero-image.svg' alt='shadow' className='card-fader' />
+      <ImageContain src='/hero-image.svg' alt='shadow' className='card-fader' />
+      <ImageContain src='/hero-image.svg' alt='shadow' className='card-fader' />
+      <ImageContain src='/hero-image.svg' alt='shadow' className='card-fader' />
+      <ImageContain src='/hero-image.svg' alt='shadow' className='card-fader' />
+      <ImageContain src='/hero-image.svg' alt='shadow' className='card-fader' />
+      <ImageContain src='/hero-image.svg' alt='shadow' className='card-fader' />
+    </>
+  );
+}
+
 export function HeroSection({ children }: PropsWithChildren) {
   return (
-    <section className={clsx("flex min-h-screen", "flex-col-reverse md:flex-row duration-150")}>
+    <section
+      className={clsx(
+        "flex min-h-screen",
+        "flex-col-reverse md:flex-row duration-150 max-w-screen overflow-hidden"
+      )}
+    >
       <div className={clsx("relative grow shrink-0 basis-1/2")}>
-        <ImageContain src='/hero-image.svg' alt='hero' />
+        <HeroImage />
       </div>
-      <div className={clsx("flex flex-col w-full self-stretch basis-1/2", "md:grow duration-150")}>
+      <div
+        className={clsx(
+          "flex flex-col w-full self-stretch basis-1/2",
+          "md:grow duration-150 relative z-20"
+        )}
+      >
         {children}
       </div>
     </section>
