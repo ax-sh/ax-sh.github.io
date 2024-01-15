375589f0a8cecdc790b53f3d0311efcb1f9a318d
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-01-15 06:02:50 +0545

    feat: add external links component

--- src/ui/past-projects-link.tsx
+++ src/ui/past-projects-link.tsx
@@ -1,6 +1,6 @@
 import React, { ComponentProps, PropsWithChildren } from "react";
 
-function ExternalLink({ children, ...props }: ComponentProps<"a">) {
+export function ExternalLink({ children, ...props }: ComponentProps<"a">) {
   return (
     <a {...props} target='_blank' rel='noreferrer'>
       {children}
@@ -8,13 +8,21 @@ function ExternalLink({ children, ...props }: ComponentProps<"a">) {
   );
 }
 
+function ExternalWorkLink({ children, ...props }: ComponentProps<"a">) {
+  return (
+    <ExternalLink className='text-green-500 hover:text-red-500 cursor-pointer' {...props}>
+      {children}
+    </ExternalLink>
+  );
+}
+
 export function PastProjectsLink({ children }: PropsWithChildren) {
   return (
-    <ExternalLink
+    <ExternalWorkLink
       className='text-green-500 hover:text-red-500 cursor-pointer'
       href='https://bit.ly/4418VWz'
     >
       {children}
-    </ExternalLink>
+    </ExternalWorkLink>
   );
 }
