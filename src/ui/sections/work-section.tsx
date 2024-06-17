524330b36f04b7e1fe1372605b43b3627dc28d46
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-06-17 22:24:33 +0545

    style: Update id attribute in WorkSection component

--- src/ui/sections/work-section.tsx
+++ src/ui/sections/work-section.tsx
@@ -5,5 +5,5 @@ import { useDataQuery } from "@/hooks/use-data-query";
 export default function WorkSection() {
   const { data } = useDataQuery();
   console.log("work", data);
-  return <section>{/*work*/}</section>;
+  return <section id={"work"}>{/*work*/}</section>;
 }
