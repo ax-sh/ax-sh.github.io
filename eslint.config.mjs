67a2c4b02010754f4c8835407c5e688ff76ad5b6
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-06-17 21:12:06 +0545

    style: Update eslint config formatting and quotes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,28 @@
-import eslint from '@eslint/js'
-import eslintConfigPrettier from 'eslint-config-prettier'
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
-import pluginSecurity from 'eslint-plugin-security'
-import sonarjs from 'eslint-plugin-sonarjs'
-import eslintPluginUnicorn from 'eslint-plugin-unicorn'
-import globals from 'globals'
-import tseslint from 'typescript-eslint'
+import eslint from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import pluginSecurity from "eslint-plugin-security";
+import sonarjs from "eslint-plugin-sonarjs";
+import eslintPluginUnicorn from "eslint-plugin-unicorn";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 const unicorn = {
   languageOptions: {
     ecmaVersion: 2024,
     globals: {
-      ...globals.node,
-    },
+      ...globals.node
+    }
   },
 
   plugins: {
-    unicorn: eslintPluginUnicorn,
+    unicorn: eslintPluginUnicorn
   },
   rules: {
-    'unicorn/better-regex': 'error',
-    '@typescript-eslint/consistent-type-imports': 'error',
-  },
-}
+    "unicorn/better-regex": "error",
+    "@typescript-eslint/consistent-type-imports": "error"
+  }
+};
 
 const eslintConfigs = [
   eslint.configs.recommended,
@@ -33,8 +33,18 @@ const eslintConfigs = [
   eslintPluginPrettierRecommended,
   eslintConfigPrettier,
   {
-    ignores: ['.config/*', 'build/', 'dist/', '.xo-config.js', '.*.js'],
-  },
-]
+    ignores: [
+      ".config/*",
+      "build/",
+      "dist/",
+      ".xo-config.js",
+      ".*.js",
+      "node_modules",
+      "public",
 
-export default eslintConfigs
+      "CHANGELOG.md"
+    ]
+  }
+];
+
+export default eslintConfigs;
