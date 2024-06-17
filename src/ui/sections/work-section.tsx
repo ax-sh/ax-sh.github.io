23b86c01223b0c68c3be38b35f7cf1f3b956449b
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-06-18 00:57:37 +0545

    feat(ui): add dynamic mock data generation for work section and update work card component styling

--- src/ui/sections/work-section.tsx
+++ src/ui/sections/work-section.tsx
@@ -1,13 +1,26 @@
 "use client";
 
 import { useDataQuery } from "@/hooks/use-data-query";
+import { faker } from "@faker-js/faker";
 
 import clsx from "clsx";
 
-const works = [false, false, false, false, false, false, false, false, false];
 
-function WorkCard() {
-  return <div className={"bg-gray-900  "}> </div>;
+type WorkCardProps = Readonly<{ label: string; src: string; url: string }>;
+function makeMockWork() {
+  return { label: faker.person.firstName(), src: faker.image.urlLoremFlickr({category:"nature"}) } as WorkCardProps;
+}
+
+const works = faker.helpers.multiple(makeMockWork, { count: 10 });
+
+function WorkCard({ src, label }: WorkCardProps) {
+  return (
+    <div className={"bg-gray-900 flex flex-col relative"}>
+      <img alt={label} src={src}  className={'object-contain flex-grow'}/>
+      <div className={'flex-grow absolute w-full flex justify-center items-end'}>
+        <span className={'text-6xl'}>{label}</span></div>
+    </div>
+  );
 }
 
 export default function WorkSection() {
@@ -15,10 +28,15 @@ export default function WorkSection() {
   console.log("work", data);
 
   return (
-    <section id={"work"} className={"h-screen flex flex-col "}>
-      <div className={clsx("container mx-auto py-8 grid gap-3 flex-grow", "grid-cols-4  ")}>
+    <section id={"work"} className={"min-h-screen flex flex-col "}>
+      <div
+        className={clsx(
+          "container mx-auto py-8 grid gap-3 flex-grow",
+          "grid-cols-1 px-4 md:grid-cols-2 lg:grid-cols-4 "
+        )}
+      >
         {works.map((i) => {
-          return <WorkCard />;
+          return <WorkCard {...i}/>;
         })}
       </div>
     </section>
