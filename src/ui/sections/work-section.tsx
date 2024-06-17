87617120f88833baf4b5af518c35bf4e64c8cd71
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-06-18 00:37:49 +0545

    feat: Add new WorkCard component to render multiple cards in WorkSection

--- src/ui/sections/work-section.tsx
+++ src/ui/sections/work-section.tsx
@@ -2,8 +2,25 @@
 
 import { useDataQuery } from "@/hooks/use-data-query";
 
+import clsx from "clsx";
+
+const works = [false, false, false, false, false, false, false, false, false];
+
+function WorkCard() {
+  return <div className={"bg-gray-900  "}> </div>;
+}
+
 export default function WorkSection() {
   const { data } = useDataQuery();
   console.log("work", data);
-  return <section id={"work"}>{/*work*/}</section>;
+
+  return (
+    <section id={"work"} className={"h-screen flex flex-col "}>
+      <div className={clsx("container mx-auto py-8 grid gap-3 flex-grow", "grid-cols-4  ")}>
+        {works.map((i) => {
+          return <WorkCard />;
+        })}
+      </div>
+    </section>
+  );
 }
