0b97329a8a5bb48d2a8cb3ef44a057d889e6458b
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-06-18 01:18:08 +0545

    style: Improve code formatting and remove unnecessary comments in WorkSection component

--- src/ui/sections/work-section.tsx
+++ src/ui/sections/work-section.tsx
@@ -2,24 +2,35 @@
 
 import { useDataQuery } from "@/hooks/use-data-query";
 import { faker } from "@faker-js/faker";
+import { className } from "postcss-selector-parser";
 
 import clsx from "clsx";
 
-
 type WorkCardProps = Readonly<{ label: string; src: string; url: string }>;
 function makeMockWork() {
-  return { label: faker.person.firstName(), src: faker.image.urlLoremFlickr({category:"nature"}) } as WorkCardProps;
+  return {
+    label: faker.person.firstName(),
+    src: faker.image.urlLoremFlickr({ category: "nature" })
+  } as WorkCardProps;
 }
 
 const works = faker.helpers.multiple(makeMockWork, { count: 10 });
 
 function WorkCard({ src, label }: WorkCardProps) {
   return (
-    <div className={"bg-gray-900 flex flex-col relative"}>
-      <img alt={label} src={src}  className={'object-contain flex-grow aspect-[4/3] items-stretch'}/>
-      <div className={'flex-grow      flex justify-center justify-items-center bg-red-500 items-stretch'}>
-        <span className={'text-6xl'}>{label}</span></div>
-    </div>
+    <a href={""}>
+      <div className={"bg-[#101010] flex flex-col relative aspect-[21/9] overflow-hidden"}>
+        <div className={clsx("flex-grow flex flex-col items-center", 'absolute inset-0 bg-black/60')}>
+          <div className={'p-1 [text-shadow:_0_3px_0_rgb(0_0_0_/_40%)] absolute right-0 bottom-0'}>
+
+          <span className={"text-6xl"}>{label}</span>
+          </div>
+        </div>
+        <img alt={label} src={src} className={clsx("object-contain", "flex-grow")} />
+        {/*<div className={'flex-grow      flex justify-center justify-items-center bg-red-500 items-stretch'}>*/}
+        {/*  </div>*/}
+      </div>
+    </a>
   );
 }
 
@@ -33,11 +44,11 @@ export default function WorkSection() {
         className={clsx(
           "container mx-auto py-8",
           "grid gap-3",
-          "grid-cols-1 px-4 md:grid-cols-2 lg:grid-cols-4 "
+          "grid-cols-1 px-4 md:grid-cols-2 lg:grid-cols-4"
         )}
       >
         {works.map((i) => {
-          return <WorkCard {...i}/>;
+          return <WorkCard {...i} />;
         })}
       </div>
     </section>
