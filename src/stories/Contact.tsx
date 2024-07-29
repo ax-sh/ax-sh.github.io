186ec5220936bffc863fc76b600e77c3e14e7c88
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-07-29 16:06:38 +0545

    style(Contact): Update input styles and integrate clsx for conditional class names

--- src/stories/Contact.tsx
+++ src/stories/Contact.tsx
@@ -4,6 +4,8 @@ import { ErrorMessage } from "@hookform/error-message";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 
+import clsx from "clsx";
+
 export type ContactFormProps = { onSubmit: SubmitHandler<contactFormForm> };
 
 // Street
@@ -58,33 +60,39 @@ export function ContactForm(props: ContactFormProps) {
         <span>Email</span>
         <input
           {...register("email")}
-          className={
-            "shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-          }
+          className={clsx(
+            "mt-1 block w-full rounded-md",
+            "text-sm text-gray-700 placeholder:text-gray-500 dark:text-gray-400 dark:placeholder:text-gray-600",
+            "border border-gray-400 focus-visible:border-transparent dark:border-gray-700 dark:bg-gray-800",
+            "focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
+          )}
         />
       </label>
       {/*{errors.name?.message && <p>{errors.name?.message}</p>}*/}
       {/*<input type="number" {...register("age", { valueAsNumber: true })} />*/}
       {/*{errors.age?.message && <p>{errors.age?.message}</p>}*/}
-      <div className='sm:col-span-2'>
-        <label
-          htmlFor='message'
-          className='block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400'
-        >
-          Your message
-        </label>
-        <textarea
-          id='message'
-          rows='6'
-          className='block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500'
-          placeholder='Leave a comment...'
-        ></textarea>
-      </div>
+      {/*<div className='sm:col-span-2'>*/}
+      {/*  <label*/}
+      {/*    htmlFor='message'*/}
+      {/*    className='block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400'*/}
+      {/*  >*/}
+      {/*    Your message*/}
+      {/*  </label>*/}
+      {/*  <textarea*/}
+      {/*    id='message'*/}
+      {/*    rows='6'*/}
+      {/*    className='block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500'*/}
+      {/*    placeholder='Leave a comment...'*/}
+      {/*  ></textarea>*/}
+      {/*</div>*/}
       <button
         type='submit'
-        className={
-          "py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-        }
+        className={clsx(
+          "inline-flex select-none justify-center rounded-md px-4 py-2 text-sm font-medium",
+          "bg-purple-600 text-white hover:bg-purple-700 dark:bg-purple-700 dark:text-gray-100 dark:hover:bg-purple-600",
+          "border border-transparent",
+          "focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
+        )}
       >
         Send
       </button>
