0403bb6617590f0b6d4eb1750be41b98402aa310
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-10-14 01:27:25 +0545

    style: Fix formatting inconsistencies in code files

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,14 +1,14 @@
-import type { MetadataRoute } from 'next';
+import type { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'Axmin Shrestha | Portfolio',
-    short_name: 'Portfolio',
-    description: 'A wip Portfolio.',
-    start_url: '/',
-    display: 'standalone',
-    background_color: '#000000',
-    theme_color: '#000000',
+    name: "Axmin Shrestha | Portfolio",
+    short_name: "Portfolio",
+    description: "A wip Portfolio.",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#000000",
+    theme_color: "#000000"
     // icons: [
     //   {
     //     src: '/icon-192x192.png',
