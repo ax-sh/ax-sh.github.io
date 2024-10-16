fe809db400292430bcf6bdbffa6f986be42c8cbc
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-10-16 17:10:27 +0545

    chore: update changelog command formatting

--- .release-it.cjs
+++ .release-it.cjs
@@ -24,7 +24,7 @@ module.exports = {
   },
   git: {
     // changelog: 'git log --pretty=format:"* %s (%h)" ${from}...${to}',
-    changelog: "nr git-cliff -u --tag v${version} -s all",
+    changelog: "nr git-cliff -u --tag 'v${version}' -s all",
     requireCleanWorkingDir: true,
     requireBranch: false,
     requireUpstream: true,
