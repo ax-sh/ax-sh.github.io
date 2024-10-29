441004de4d3257e98efaf5cb3c9555c5efc66f53
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-10-30 04:50:52 +0545

    chore(eslint): update ESLint configuration file structure

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,65 +1,78 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
 import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
 });
 
 export default [
-    ...compat.extends("next/core-web-vitals", "next", "airbnb", "./.prettier.eslintrc.cjs"),
-    {
-        plugins: {
-            "@typescript-eslint": typescriptEslint,
-        },
-
-        rules: {
-            "@typescript-eslint/ban-ts-comment": "warn",
-            "react/react-in-jsx-scope": "off",
-            "react/jsx-filename-extension": "off",
-            "react/jsx-props-no-spreading": "off",
-            "import/extensions": "off",
-            "import/prefer-default-export": "off",
-            "no-shadow": "off",
-            "import/no-extraneous-dependencies": "off",
-            "import/no-amd":"off"
-        },
+  ...compat.extends("next/core-web-vitals", "next", "airbnb", "./.prettier.eslintrc.cjs"),
+  {
+    plugins: {
+      "@typescript-eslint": typescriptEslint
     },
-    {
-        files: ["**/*.config.{ts,js}"],
 
-        rules: {
-            "global-require": "off",
-        },
-    },
-    {
-        files: ["**/*.test.{ts,tsx}"],
-
-        rules: {
-            "no-undef": "off",
-
-            "import/no-extraneous-dependencies": ["error", {
-                devDependencies: ["**/*.test.ts", "**/*.test.tsx"],
-            }],
-        },
-    },
-    {
-        ignores: [
-            ".config/*",
-            "build/",
-            "dist/",
-            ".xo-config.js",
-            ".*.js",
-            "node_modules",
-            "public",
-            ".next",
-            "CHANGELOG.md"
-        ]
+    rules: {
+      "@typescript-eslint/ban-ts-comment": "warn",
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-filename-extension": "off",
+      "react/jsx-props-no-spreading": "off",
+      "import/extensions": "off",
+      "import/prefer-default-export": "off",
+      "no-shadow": "off",
+      "import/no-extraneous-dependencies": "off",
+      "import/no-amd": "off"
     }
+  },
+  {
+    files: ["**/.storybook/*.{ts,js}"],
+
+    rules: {
+      "import/no-mutable-exports": "off"
+    }
+  },
+  {
+    files: ["**/*.config.{ts,js}", ".release-it.cjs"],
+
+    rules: {
+      "global-require": "off",
+      "import/newline-after-import": "off"
+    }
+  },
+  {
+    files: ["**/*.test.{ts,tsx}"],
+
+    rules: {
+      "no-undef": "off",
+
+      "import/no-extraneous-dependencies": [
+        "error",
+        {
+          devDependencies: ["**/*.test.ts", "**/*.test.tsx"]
+        }
+      ]
+    }
+  },
+  {
+    ignores: [
+      ".config/*",
+      "build/",
+      "dist/",
+      ".xo-config.js",
+      ".*.js",
+      "node_modules",
+      "public",
+      ".next",
+      "CHANGELOG.md",
+      ".prettier.eslintrc.cjs",
+      "____eslint.config.mjs"
+    ]
+  }
 ];
